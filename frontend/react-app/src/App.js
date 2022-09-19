3e64dc80d8b6dbb86dad984ff5200764123afaba
Adeola Fadare <adeolafadare8@gmail.com>
2022-09-20 00:25:55 +0100

    added some logos started header markup started navbar

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useEffect } from "react";
 
 import './App.css';
 import HomePage from "./pages/Home";
 import Page404 from "./pages/404";
 import Header from "./components/Header";
 function App() {
+  //var theme = useState(localStorage.getItem('userTheme') ? localStorage.getItem('userTheme') : 'light', setTheme)
   return (
-    <React.Fragment>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={
-            <React.Fragment>
-              <Header page="home" />
-              <HomePage />
-            </React.Fragment>
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={
+          <React.Fragment>
+            <HomePage />
+          </React.Fragment>
 
-          } />
-          <Route path="*" element={
-            <Page404 />
-          } />
-        </Routes>
-      </Router>
-    </React.Fragment>
+        } />
+        <Route path="*" element={
+          <Page404 />
+        } />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/react-app/src/components/Header.jsx
+++ frontend/react-app/src/components/Header.jsx
@@ -1,17 +1,90 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
-  state = {
-    currentPage: this.props.page,
-  };
   render() {
     return (
       <header id="site-header">
-        <h1>Header</h1>
-        <p>{this.state.currentPage}</p>
+        <div className="icon">
+          <img src="icons/logo.svg" alt="logo" />
+        </div>
+        <Search />
+        <ToggleTheme />
+        <div className="header-nav">
+          <nav>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : null)}
+            >
+              Home
+            </NavLink>
+          </nav>
+          <nav>
+            <NavLink
+              to="/categories"
+              className={({ isActive }) => (isActive ? "active" : null)}
+            >
+              Categories
+            </NavLink>
+          </nav>
+          <nav>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? "active" : null)}
+            >
+              About
+            </NavLink>
+          </nav>
+          <nav>
+            <NavLink
+              to="/contact-us"
+              className={({ isActive }) => (isActive ? "active" : null)}
+            >
+              Contact Us
+            </NavLink>
+          </nav>
+          <nav>
+            <NavLink
+              to="/write-for-us"
+              className={({ isActive }) => (isActive ? "active" : null)}
+            >
+              Write for us
+            </NavLink>
+          </nav>
+        </div>
       </header>
     );
   }
 }
-
+class Search extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="search">
+        <div className="search-box">
+          <span></span>
+          <span>
+            <input
+              type="text"
+              name="blog-search"
+              id="blog-search"
+              placeholder="search"
+            />
+          </span>
+        </div>
+        <div className="search-results"></div>
+      </div>
+    );
+  }
+}
+function ToggleTheme() {
+  return (
+    <div className="toggle-theme">
+      <span>
+        <img src="icons/theme-dark.svg" alt="theme-icon" />
+        <img src="icons/theme-light.svg" alt="theme-icon" />
+      </span>
+    </div>
+  );
+}
 export default Header;
