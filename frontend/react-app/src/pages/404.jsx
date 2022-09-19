43e7f75883702212d002c066ecbd453f380e6e87
Adeola Fadare <adeolafadare8@gmail.com>
2022-09-19 20:52:42 +0100

    Added home page and 404 page

---
+++ frontend/react-app/src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Page404 extends Component {
+  state = {};
+  render() {
+    return <h1>Page not Found</h1>;
+  }
+}
+
+export default Page404;
---
+++ frontend/react-app/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class HomePage extends Component {
+  state = {};
+  render() {
+    return <h1>Home Page</h1>;
+  }
+}
+
+export default HomePage;
