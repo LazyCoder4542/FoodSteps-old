3ce365575449fa660d68f6acd88aa6a2548b5856
Adeola Fadare <adeolafadare8@gmail.com>
2022-09-22 18:38:19 +0100

    quick edit

--- frontend/react-app/src/pages/Home.jsx
+++ frontend/react-app/src/pages/Home.jsx
@@ -48,6 +48,99 @@ class HomePage extends Component {
                   </div>
                 </div>
               </div>
+              <div className="card">
+                <div className="post-img">
+                  <img src="images/test1.png" alt="" />
+                </div>
+                <div className="post-details">
+                  <h2 className="post-title">How to Cook</h2>
+                  <p className="post-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Similique, nobis!
+                  </p>
+                  <div className="post-author">
+                    By&nbsp;<span className="author">Food Arts</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="popular">
+          <header>
+            <h1>Popular Posts</h1>
+          </header>
+          <div className="wrapper">
+            <div className="post-cards">
+              <div className="card">
+                <div className="post-img">
+                  <img src="images/test1.png" alt="" />
+                </div>
+                <div className="post-details">
+                  <h2 className="post-title">How to Cook</h2>
+                  <p className="post-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Similique, nobis!
+                  </p>
+                  <div className="post-author">
+                    By&nbsp;<span className="author">Food Arts</span>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="post-img">
+                  <img src="images/test1.png" alt="" />
+                </div>
+                <div className="post-details">
+                  <h2 className="post-title">How to Cook</h2>
+                  <p className="post-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Similique, nobis!
+                  </p>
+                  <div className="post-author">
+                    By&nbsp;<span className="author">Food Arts</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="team">
+          <header>
+            <h1>Meet the team</h1>
+          </header>
+          <div className="wrapper">
+            <div className="post-cards">
+              <div className="card">
+                <div className="post-img">
+                  <img src="images/test1.png" alt="" />
+                </div>
+                <div className="post-details">
+                  <h2 className="post-title">How to Cook</h2>
+                  <p className="post-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Similique, nobis!
+                  </p>
+                  <div className="post-author">
+                    By&nbsp;<span className="author">Food Arts</span>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="post-img">
+                  <img src="images/test1.png" alt="" />
+                </div>
+                <div className="post-details">
+                  <h2 className="post-title">How to Cook</h2>
+                  <p className="post-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Similique, nobis!
+                  </p>
+                  <div className="post-author">
+                    By&nbsp;<span className="author">Food Arts</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
